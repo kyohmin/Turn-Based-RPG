bea0cb4dea95090059bf5e1288980ae60a48ccfa
Kyoungho Min <khms@KHMSs-Macbook-Pro.local>
2020-10-25 10:45:51 +0900

    Update game_t2.py
    Changed variable names and made CLI more comprehensible.

--- game_t2/game_t2.py
+++ game_t2/game_t2.py
@@ -1,14 +1,17 @@
 # Import Modules
 
-# Classes
-class Character_stat:
+# Classes==============================================
+
+# Unit Common Stat
+class Unit:
     ally_counter = 0
     enemy_counter = 0
 
+    # Initialize
     def __init__(self, NAME, TEAM):
         self.TEAM = ""
-        self.NAME = ""
         self.RACE = ""
+        self.NAME = ""
         self.HP = 0
         self.ATK = 0
         self.DEF = 0
@@ -27,6 +30,7 @@ class Character_stat:
     def set_race(self, RACE):
         self.RACE = RACE
 
+    # Attack Function
     def attack(self, TARGET):
         if self.ALIVE == True:
             if self.TEAM == TARGET.TEAM:
@@ -44,10 +48,11 @@ class Character_stat:
         else:
             print("You can't attack when you are dead")
 
-    def show(self):
+    # Stats of the unit
+    def stat(self):
         return self.RACE, self.NAME, self.HP, self.ATK, self.DEF, self.TEAM, self.RANK, self.ALIVE
 
-class Ogre(Character_stat):
+class Ogre(Unit):
     def __init__(self):
         self.HP = 40
         self.ATK = 7
@@ -69,7 +74,7 @@ class Ogre(Character_stat):
             self.DEF = 5
             self.RANK = 3
 
-class Knight(Character_stat):
+class Knight(Unit):
     def __init__(self):
         self.HP = 45
         self.ATK = 5
@@ -90,13 +95,24 @@ class Knight(Character_stat):
             self.DEF = 8
             self.RANK = 3
 
-class Sorcerer(Character_stat):
+class Sorcerer(Unit):
     def __init__(self):
         self.HP = 30
         self.ATK = 3
         self.DEF = 2
         self.RANK = 1
         self.ALIVE = True
+
+    # Magic
+    def magic(self):
+        def cure(self):
+            pass
+        
+        def poison(self):
+            pass
+
+        def freeze(self):
+            pass
 
     def rank_up(self):
         if self.EXP >= 40 and self.RANK == 1:
@@ -111,181 +127,211 @@ class Sorcerer(Character_stat):
             self.DEF = 5
             self.RANK = 3
 
-# Functions
-def inputChecker(Choice, End, Message):
-    while not Choice.isdigit() or int(Choice) <= 0 or int(Choice) > End:
-        print("\n=============Wrong Value==============")
-        print("\nYou entered wrong value, Please Try again\n")
-        print(Message)
-        Choice = input("Choose one : ")
-    return int(Choice)
+# Functions====================================================
+def inputChecker(INPUT, END, MESSAGE):
+    while not INPUT.isdigit() or int(INPUT) <= 0 or int(INPUT) > END:
+        top_line("WRONG VALUE")
+        print("You entered wrong value, Please Try again")
+        print(MESSAGE)
+        INPUT = input("Choose one : ")
+    return int(INPUT)
 
+def top_line(TITLE):
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n==== " + TITLE +" " +"=" * (43 - len(TITLE)))
 
-# ==============Main Menu=================
+def turn_line(NUMBER, PLAYING):
+    print("\n==== "+"TURN "+ str(NUMBER) +" ["+ PLAYING +"] "+"=" * (35 - (len(str(NUMBER))+ len(PLAYING))))
+
+# Settings=====================================================
+
+# Start Menu
 def start_menu():
     global start
 
-    print("===============Main menu==============\n")
-    print("Welcome to the game!\n")
-    print("1. Start the Game\n2. Look at the save files\n")
+    top_line("MAIN MENU")
+    print("Welcome to the game!")
+    print("\n1. Start the Game\n2. Look at the save files\n")
     start = input("Choose one : ")
-    start = inputChecker(start, 2, "1. Start the Game\n2. Quit\n")
+    start = inputChecker(start, 2, "\n1. Start the Game\n2. Quit\n")
 
+# Opponent, # of Players, and Race selection
 def setting_game():
     global type_game
     global num_player
-    global numPlayer
-    global numEnemy
-    numPlayer = []
-    numEnemy = []
+    global objs_ally
+    global objs_enemy
+    objs_ally = []
+    objs_enemy = []
 
-    print("\n============Fight Enemy=============\n")
-    print("1. Player vs Player\n2. Player vs A.I.\n")
+    top_line("OPPONENT SETTING")
+    print("\n1. Player vs Player\n2. Player vs A.I.\n")
     type_game = input("Choose one : ")
-    type_game = inputChecker(type_game, 2, "1. Player vs Player\n2. Player vs A.I.\n")
+    type_game = inputChecker(type_game, 2, "\n1. Player vs Player\n2. Player vs A.I.\n")
 
-    print("\n==========# of Players==============\n")
-    print("1. 1vs1\n2. 2vs2\n3. 3vs3\n4. 4vs4\n5. 5vs5\n")
+    top_line("# OF PLAYERS")
+    print("\n1. 1vs1\n2. 2vs2\n3. 3vs3\n4. 4vs4\n5. 5vs5\n")
     num_player = input("Choose one : ")
-    num_player = inputChecker(num_player, 5, "1. 1vs1\n2. 2vs2\n3. 3vs3\n4. 4vs4\n5. 5vs5\n")
+    num_player = inputChecker(num_player, 5, "\n1. 1vs1\n2. 2vs2\n3. 3vs3\n4. 4vs4\n5. 5vs5\n")
     
     # Making list of Players
     for units in range(num_player):
         player = f'player_{str(units+1)}'
         enemy = f'enemy_{str(units+1)}'
-        numPlayer.append(player)
-        numEnemy.append(enemy)
+        objs_ally.append(player)
+        objs_enemy.append(enemy)
 
     # Object setting [Player Side]
     for num in range(num_player):
         temp_1 = num
-        print("\n=========Choose race of your team============\n")
-        print("1. Ogre\n\tOgres have strong power, but have low accuracy\n")
-        print("2. Knight\n\tKnight have decent power and strong defence power\n")
-        print("3. Sorcerer\n\tSorcerer have weak body, but they can cast powerful magics\n")
+        top_line("CHOOSE THE RACE OF YOUR [ALLY]")
+        print("\n1. [Ogre]\n\tOgres have strong power, but have low accuracy\n")
+        print("2. [Knight]\n\tKnight have decent power and strong defence power\n")
+        print("3. [Sorcerer]\n\tSorcerer have weak body, but they can cast powerful magics\n")
         num = input("Choose one : ")
-        num = inputChecker(num, 3, "1. Ogre\n\tOgres have strong power, but have low accuracy\n\n2. Knight\n\tKnight have decent power and strong defence power\n\n3. Sorcerer\n\tSorcerer have weak body, but they can cast powerful magics\n")
+        num = inputChecker(num, 3, "\n1. [Ogre]\n\tOgres have strong power, but have low accuracy\n\n2. [Knight]\n\tKnight have decent power and strong defence power\n\n3. [Sorcerer]\n\tSorcerer have weak body, but they can cast powerful magics\n")
         if num == 1:
-            numPlayer[temp_1] = Ogre()
-            numPlayer[temp_1].set_race("Ogre")
+            objs_ally[temp_1] = Ogre()
+            objs_ally[temp_1].set_race("Ogre")
         elif num == 2:
-            numPlayer[temp_1] = Knight()
-            numPlayer[temp_1].set_race("Knight")
+            objs_ally[temp_1] = Knight()
+            objs_ally[temp_1].set_race("Knight")
         else:
-            numPlayer[temp_1] = Sorcerer()
-            numPlayer[temp_1].set_race("Sorcerer")
+            objs_ally[temp_1] = Sorcerer()
+            objs_ally[temp_1].set_race("Sorcerer")
 
     # Object setting [Enemy Side]
     for num in range(num_player):
         temp_1 = num
-        print("\n=========Choose the race of the enemy============\n")
-        print("1. Ogre\n\tOgres have strong power, but have low accuracy\n")
-        print("2. Knight\n\tKnight have decent power and strong defence power\n")
-        print("3. Sorcerer\n\tSorcerer have weak body, but they can cast powerful magics\n")
+        top_line("CHOOSE THE RACE OF YOUR [ENEMY]")
+        print("\n1. [Ogre]\n\tOgres have strong power, but have low accuracy\n")
+        print("2. [Knight]\n\tKnight have decent power and strong defence power\n")
+        print("3. [Sorcerer]\n\tSorcerer have weak body, but they can cast powerful magics\n")
         num = input("Choose one : ")
-        num = inputChecker(num, 3, "1. Ogre\n\tOgres have strong power, but have low accuracy\n\n2. Knight\n\tKnight have decent power and strong defence power\n\n3. Sorcerer\n\tSorcerer have weak body, but they can cast powerful magics\n")
+        num = inputChecker(num, 3, "\n1. [Ogre]\n\tOgres have strong power, but have low accuracy\n\n2. [Knight]\n\tKnight have decent power and strong defence power\n\n3. [Sorcerer]\n\tSorcerer have weak body, but they can cast powerful magics\n")
         if num == 1:
-            numEnemy[temp_1] = Ogre()
-            numEnemy[temp_1].set_race("Ogre")
+            objs_enemy[temp_1] = Ogre()
+            objs_enemy[temp_1].set_race("Ogre")
         elif num == 2:
-            numEnemy[temp_1] = Knight()
-            numEnemy[temp_1].set_race("Knight")
+            objs_enemy[temp_1] = Knight()
+            objs_enemy[temp_1].set_race("Knight")
         else:
-            numEnemy[temp_1] = Sorcerer()
-            numEnemy[temp_1].set_race("Sorcerer")
+            objs_enemy[temp_1] = Sorcerer()
+            objs_enemy[temp_1].set_race("Sorcerer")
 
-
-def naming_units():
+    # Naming the units
     for num in range(num_player):
-        print("\n=============Name TEAM===================\n")
-        name = input(f'Please enter the name of the player_{num+1} : ')
+        top_line("NAME YOUR [PLAYER]")
+        name = input(f'\nPlease enter the name of the player_{num+1} : ')
         name = name.strip()
         while name == '':
-            print("\n=============Wrong Value===================\n")
-            name = input(f'Please enter the name of the player_{num+1} : ')
+            top_line("WRONG VALUE")
+            name = input(f'\nPlease enter the name of the player_{num+1} : ')
             name = name.strip()
-        player = numPlayer[num]
+        player = objs_ally[num]
         player.set_name(name)
         # Set TEAM
         player.set_team("ALLY")
-        Character_stat.ally_counter += 1
+        Unit.ally_counter += 1
 
     for num in range(num_player):
-        print("\n=============Name Enemy===================\n")
-        name = input(f'Please enter the name of the Enemy_{num+1} : ')
+        top_line("NAME YOUR [OPPONENT]")
+        name = input(f'\nPlease enter the name of the Enemy_{num+1} : ')
         name = name.strip()
         while name == '':
-            print("\n=============Wrong Value===================\n")
-            name = input(f'Please enter the name of the Enemy_{num+1} : ')
+            top_line("WRONG VALUE")
+            name = input(f'\nPlease enter the name of the Enemy_{num+1} : ')
             name = name.strip()
-        player = numEnemy[num]
+        player = objs_enemy[num]
         player.set_name(name)
         # Set TEAM
         player.set_team("ENEMY")
-        Character_stat.enemy_counter += 1
+        Unit.enemy_counter += 1
 
+# GAME ==========================================================
 def show_stats():
-    print("\n===========Players' Stats================\n")
+    top_line("PLAYER'S STAT")
     for a in range(num_player):
-        player = numPlayer[a].show()
+        player = objs_ally[a].stat()
         if player[7] == True:
-            print("RACE : {0}, NAME : {1}, HP : {2}, ATK : {3}, DEF : {4}, TEAM : {5}, RANK : {6}" \
+            print("\nRACE : {0} / NAME : {1} / HP : {2} / ATK : {3} / DEF : {4} / TEAM : {5} / RANK : {6}" \
                 .format(player[0], player[1], player[2], player[3], player[4], player[5], player[6]))
         else:
-            print("{0} is DEAD" .format(player[1]))
+            print("\n{0} is DEAD" .format(player[1]))
+    
     print()
+
     for a in range(num_player):
-        enemy = numEnemy[a].show()
+        enemy = objs_enemy[a].stat()
         if enemy[7] == True:
-            print("RACE : {0}, NAME : {1}, HP : {2}, ATK : {3}, DEF : {4}, TEAM : {5}, RANK : {6}" \
+            print("\nRACE : {0} / NAME : {1} / HP : {2} / ATK : {3} / DEF : {4} / TEAM : {5} / RANK : {6}" \
                 .format(enemy[0], enemy[1], enemy[2], enemy[3], enemy[4], enemy[5], enemy[6]))
         else:
-            print("{0} is DEAD" .format(enemy[1]))
+            print("\n{0} is DEAD" .format(enemy[1]))
+
+def play():
+    GAME = True
+    turn = 1
+    if start == 1:
+        while GAME:
+            # Exit the game if ther is noone left
+            if Unit.ally_counter == 0 or Unit.enemy_counter == 0:
+                GAME = False
+            print(f"\nTURN : {turn}")
+            show_stats()
+
+            # ALLY FIRST
+            turn_line(turn, "ALLY'S TURN")
+            print()
+            for a in range(num_player):
+                print(str(a+1) + ".", objs_ally[a].stat()[1] + " (" + objs_ally[a].stat()[0] + ")")
+            player = input("\nChoose your player : ")
+            while not player.isdigit() or int(player) < 0 or int(player) > num_player:
+                for a in range(num_player):
+                    print(str(a+1) + ".", objs_ally[a].stat()[1] + " (" + objs_ally[a].stat()[0] + ")")
+                player = input("\nWho would you attack? : ")
+            player = int(player)
+
+            for a in range(num_player):
+                print(str(a+1) + ".", objs_enemy[a].stat()[1] + " (" + objs_enemy[a].stat()[0] + ")")
+            enemy = input("\nWho would you attack? : ")
+            while not enemy.isdigit() or int(enemy) < 0 or int(enemy) > num_player:
+                for a in range(num_player):
+                    print(str(a+1) + ".", objs_enemy[a].stat()[1] + " (" + objs_enemy[a].stat()[0] + ")")
+                enemy = input("\nWho would you attack? : ")
+            enemy = int(enemy)
+            objs_ally[player-1].attack(objs_enemy[enemy-1])
+
+            show_stats()
+
+            # ENEMY'S TURN
+            turn_line(turn, "ENEMY'S TURN")
+            print()
+            for a in range(num_player):
+                print(str(a+1) + ".", objs_enemy[a].stat()[1] + " (" + objs_enemy[a].stat()[0] + ")")
+            player = input("\nChoose your player : ")
+            while not player.isdigit() or int(player) < 0 or int(player) > num_player:
+                for a in range(num_player):
+                    print(str(a+1) + ".", objs_enemy[a].stat()[1] + " (" + objs_enemy[a].stat()[0] + ")")
+                player = input("\nWho would you attack? : ")
+            player = int(player)
+            print()
+            for a in range(num_player):
+                print(str(a+1) + ".", objs_ally[a].stat()[1] + " (" + objs_ally[a].stat()[0] + ")")
+            enemy = input("\nWho would you attack? : ")
+            while not enemy.isdigit() or int(enemy) < 0 or int(enemy) > num_player:
+                for a in range(num_player):
+                    print(str(a+1) + ".", objs_ally[a].stat()[1] + " (" + objs_ally[a].stat()[0] + ")")
+                enemy = input("\nWho would you attack? : ")
+            enemy = int(enemy)
+            objs_enemy[player-1].attack(objs_ally[enemy-1])
+
+            turn += 1
 
 
 # Start
 def main():
-    GAME = True
-    turn = 1
     start_menu()
-    if start == 1:
-        setting_game()
-        naming_units()
-        while GAME:
-            # Exit the game if ther is noone left
-            if Character_stat.ally_counter == 0 or Character_stat.enemy_counter == 0:
-                GAME = False
-            print(f"\nTURN : {turn}")
-            show_stats()
-            if (turn % 2) == 1:
-                print("\n==============Player Turn==============\n")
-                for a in range(num_player):
-                    print(str(a+1) + ".", numPlayer[a].show()[1] + " (" + numPlayer[a].show()[0] + ")")
-                player = input("\nChoose your player : ")
-                player = inputChecker(player, num_player, "OOPS")
-                player = int(player)
-                print(Character_stat.ally_counter, Character_stat.enemy_counter)
-                for a in range(num_player):
-                    print(str(a+1) + ".", numEnemy[a].show()[1] + " (" + numEnemy[a].show()[0] + ")")
-                enemy = input("\nWho would you attack? : ")
-                enemy = inputChecker(enemy, num_player, "OOPS")
-                enemy = int(enemy)
-                numPlayer[player-1].attack(numEnemy[enemy-1])
-
-            else:
-                print("\n==============Enemy Turn==============\n")
-                for a in range(num_player):
-                    print(str(a+1) + ".", numEnemy[a].show()[1] + " (" + numEnemy[a].show()[0] + ")")
-                player = input("\nChoose your player : ")
-                player = inputChecker(player, num_player, "OOPS")
-                player = int(player)
-                print()
-                for a in range(num_player):
-                    print(str(a+1) + ".", numPlayer[a].show()[1] + " (" + numPlayer[a].show()[0] + ")")
-                enemy = input("\nWho would you attack? : ")
-                enemy = inputChecker(enemy, num_player, "OOPS")
-                enemy = int(enemy)
-                numEnemy[player-1].attack(numPlayer[enemy-1])
-
-            turn += 1
+    setting_game()
+    play()
+    
 main()
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print(2%2)
+def top_line(TITLE):
+    print("==== " + TITLE +" " +"=" * (43 - len(TITLE)))
+
+def turn_line(NUMBER, PLAYING):
+    print("==== "+"TURN "+ str(NUMBER) +" ["+ PLAYING +"] "+"=" * (35 - (len(str(NUMBER))+ len(PLAYING))))
+
+turn = 1
+top_line("MAIN MENU")
+turn_line(turn, "ALLY'S TURN")
+turn = 10
+turn_line(turn, "PLAYER'S TURN")
