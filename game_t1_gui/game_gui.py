e565f523edfe0c10add80bb0c3e729befef1f072
Kyoungho Min <khms@KHMSs-Macbook-Pro.local>
2020-11-21 20:05:15 +0900

    fixed little bugs in gui file

--- game_t1_gui/game_gui.py
+++ game_t1_gui/game_gui.py
@@ -10,26 +10,6 @@ pygame.init()
 pygame.display.set_caption('Turn Based RPG - T1')
 screen = pygame.display.set_mode((900,600))
 
-<<<<<<< HEAD
-# Game Functions ==============================================
-def main_menu_page(): 
-    # Load Images
-    box_size = (355, 52)
-    setting_size = (71, 65)
-    bg = pygame.image.load('./Game_Assets/Backgrounds/menu_background.png')
-    bg = pygame.transform.scale(bg,(900,600))
-    logo = pygame.image.load('./Game_Assets/Intro_Page/Turn_based_RPG_banner.png')
-    start_btn = pygame.image.load('./Game_Assets/Intro_Page/Start_Button.png')
-    # start_btn = pygame.transform.scale(start_btn, box_size)
-    saved_btn = pygame.image.load('./Game_Assets/Intro_Page/Saved_Files.png')
-    saved_btn = pygame.transform.scale(saved_btn, box_size)
-    exit_btn = pygame.image.load('./Game_Assets/Intro_Page/Exit_Button.png')
-    exit_btn = pygame.transform.scale(exit_btn, box_size)
-    setting_btn = pygame.image.load('./Game_Assets/Intro_Page/Settings_Icon.png')
-    setting_btn = pygame.transform.scale(setting_btn, setting_size)
-
-    # Variables
-=======
 def main_menu_page():
     # Background Image
     box_size = (358,52)
@@ -43,46 +23,10 @@ def main_menu_page():
     quit_btn = pygame.transform.scale(quit_btn,box_size)
 
     change_click = False
->>>>>>> 0734a854846500a0abccfb1068ad6c3e85bc2abc
     click = False
     # While Loop
     while True:
         mx, my = pygame.mouse.get_pos()
-<<<<<<< HEAD
-        print(mx, my)
-        print("pointer =", point)
-
-        # Show Images
-        screen.blit(bg,(0,0))
-        screen.blit(logo,(190,-10))
-        screen.blit(start_btn,(289,261))
-        screen.blit(saved_btn,(289,338))
-        screen.blit(exit_btn,(289,417))
-        screen.blit(setting_btn,(805,510))
-
-        if not 289 <= mx <= 289+355 and  not 261 <= my <= 261+52: point = 0
-        if not 289 <= mx <= 289+355 and not 338 <= my <= 338+52: point = 0
-        if not 289 <= mx <= 289+355 and not 417 <= my <= 417+52: point = 0
-        if not 805 <= mx <= 805+71 and not 510 <= my <= 510+65: point = 0
-
-        if 289 <= mx <= 289+355 and 261 <= my <= 261+52 or point == 1: # Play
-            point = 1
-            if click or enter:
-                enemy_selection_page()
-        if 289 <= mx <= 289+355 and 338 <= my <= 338+52 or point == 2: # Save / Load
-            point = 2
-            if click or enter:
-                save_load_page()
-        if 289 <= mx <= 289+355 and 417 <= my <= 417+52 or point == 3: # Quit
-            point = 3
-            if click or enter:
-                pygame.quit()
-                sys.exit()
-        if 805 <= mx <= 805+71 and 510 <= my <= 510+65 or point == 4: # Setting
-            point = 4
-            if click or enter:
-                setting_page()
-=======
         screen.blit(bg,(0,0))
         screen.blit(start_btn,(287,260))
         screen.blit(saved_btn,(287,338))
@@ -108,7 +52,6 @@ def main_menu_page():
         elif dest_setting:
             setting_page()
         
->>>>>>> 0734a854846500a0abccfb1068ad6c3e85bc2abc
 
         click = False
         # Event Collections
@@ -117,25 +60,11 @@ def main_menu_page():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-<<<<<<< HEAD
-            if event.type == KEYDOWN:
-                if event.key == K_DOWN:
-                    if 0 <= point <= 3:
-                        point += 1
-                if event.key == K_UP:
-                    if 2 <= point <= 4:
-                        point -= 1
-                if event.key == K_RETURN:
-                    enter = True
-                    
-            if event.type == MOUSEBUTTONDOWN:
-=======
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     change_click = True
             if event.type == MOUSEBUTTONUP:
                 change_click = False
->>>>>>> 0734a854846500a0abccfb1068ad6c3e85bc2abc
                 if event.button == 1:
                     click = True
 
@@ -143,14 +72,6 @@ def main_menu_page():
         clock.tick(120)
 
 def enemy_selection_page():
-<<<<<<< HEAD
-    bg = pygame.image.load('./Game_Assets/Backgrounds/menu_background.png')
-    bg = pygame.transform.scale(bg,(900,600))  
-    473,166
-    logo = pygame.image.load('./Game_Assets/Intro_Page/Turn_based_RPG_banner.png')
-    logo = pygame.transform.scale(logo,(500,260))
-
-=======
     box_size = (400,400)
     bg = pygame.image.load(resource_path('./Game_Assets/Backgrounds/menu_background.png'))
     bg = pygame.transform.scale(bg,(900,600))
@@ -162,7 +83,6 @@ def enemy_selection_page():
     pvai_btn = pygame.transform.scale(pvai_btn,box_size)
 
     change_click = False
->>>>>>> 0734a854846500a0abccfb1068ad6c3e85bc2abc
     click = False
     while True:
         mx, my = pygame.mouse.get_pos()
